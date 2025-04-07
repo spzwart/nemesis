61d11ea7da651d829256bfc1380ef35fc85f5d69
ErwanH29 <e.j.s.g.hochart@umail.leidenuniv.nl>
2025-04-07 14:52:39 +0200

    Syncing with validated code

--- src/globals.py
+++ src/globals.py
@@ -1,14 +1,15 @@
+import time
 from amuse.ext.galactic_potentials import MWpotentialBovy2015
 from amuse.lab import constants, units
 
-ASTEROID_RADIUS = 100. | units.km
-CONNECTED_COEFF = 2.
 EPS = 1.e-8
 GRAV_THRESHOLD = 5.e-3
 MIN_EVOL_MASS = 0.08 | units.MSun
-PARENT_RADIUS_COEFF = 1000. | units.au
-PARENT_RADIUS_MAX = 5000 | units.au
+PARENT_RADIUS_COEFF = 500. | units.au
+PARENT_RADIUS_MAX = 2500 | units.au
+
 SI_UNITS = (1. | units.kg * units.m**-2.) * constants.G
+START_TIME = time.time()
 
 
 MWG = MWpotentialBovy2015()
